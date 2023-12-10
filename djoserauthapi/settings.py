c0ae3798ff588b66505fd28e18524049002a21b5
Atia-Khan <127720791+Atia-Khan@users.noreply.github.com>
2023-12-10 17:49:08 +0500

    Update settings.py

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware’,
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djoserauthapi.urls'
