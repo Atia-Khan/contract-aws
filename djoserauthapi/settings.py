27a71ec0565b49cb79f3ff1131127ec8c7082ccd
Atia-Khan <127720791+Atia-Khan@users.noreply.github.com>
2023-12-10 19:19:26 +0500

    Update settings.py

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -271,7 +271,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 # Or * to allow all
 ALLOWED_HOSTS = ['*']
 
